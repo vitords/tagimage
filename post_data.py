3bbd63b8adbce6620bd930f22c7f3586110a88ac
Vitor <vitords.42@gmail.com>
2017-08-23 21:53:23 -0300

    Easier access to change YouTube API key

--- post_data.py
+++ post_data.py
@@ -1,8 +1,10 @@
 import requests
 
+api_key = 'YOUTUBE API KEY HERE'
+
 api_url = 'http://127.0.0.1:5000/search'
 # max_videos can't be larger than 50 as YouTube won't accept it
-payload = {'topic': '/m/068hy', 'max_videos': 50, 'region_code': 'US', 'key': 'YOUTUBE API KEY HERE'}
+payload = {'topic': '/m/068hy', 'max_videos': 50, 'region_code': 'US', 'key': api_key}
 
 response = requests.post(api_url, data=payload)
 print(response.text)
